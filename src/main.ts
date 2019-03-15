eca5f53134b7319377e96ca3c971fc511e4b2abb
ningzhang <15188399413@163.com>
2019-03-15 17:05:00 +0800

    总结.d.ts的类型检查使用

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,12 @@ Vue.prototype.$axios=axios;
  3 一是挂在到window上，来防止第一步，把axios挂在在window对象上时，报类型错误、到了这一步，axios,其实已经可以发送请求了
  4 二是声明axios的全局类型在global对象上，来防止axios单独存在的时候报错。
 */ 
+
+/*
+ ts中declare是相当于js中的var等，因此声明函数，接口，对象等都需要加上它
+ 类型检查其实是为编辑器提供静态类型检查，普通的h5，编辑.d.ts一样可以达到提示作用，但无法使用类型报错，因为没有指明语言类型
+ .d.ts是为js提供服务的，因此文件应该放在与js同级的位置，编辑器会自动检查的。
+*/ 
 window.axios = axios;
 
 import ElementUI from 'element-ui';
